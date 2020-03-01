b24c429ad0a28c549eaa0f426c60a0148652f6b5
Sean K. Roth <seanr@communitybenchmark.com>
2020-02-29 17:08:44 -0800

    Beginning of the pokemon detail processor
    - adds base processor that pulls out type information
    - adds a helper fn that produces a map of header name to table element, which will be useful for pulling out other metadata. (e.g. to get the locations of the pokemon in the games

--- src/clj/dex/scrape.clj
+++ src/clj/dex/scrape.clj
@@ -1,10 +1,31 @@
 (ns dex.scrape
-  (:require [net.cgrand.enlive-html :refer [select attr? text emit* has pred first-child last-child nth-child]]
+  (:require [net.cgrand.enlive-html :refer [select but attr? text emit* has pred first-child last-child nth-child]]
             [skyscraper.core :as core :refer [defprocessor]]
             [skyscraper.enlive-helpers :refer [href]])) 
 
 (def seed [{:url "https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_name"
             :processor :pokemon-list}])
+
+(def type-selector
+  [:table.roundy :td.roundy :table.roundy [:td (but (attr? :style))] :table :a])
+
+(defn tables-by-header
+  "Takes a vector of mixed table and header elements and returns
+  a map of the header text to the associated table"
+  [elements]
+  (loop [els elements
+         last-header nil
+         table-map {}]
+    (let [el (first els)
+          tag (:tag el)
+          next-els (rest els)]
+      (if (nil? el)
+        table-map
+        (condp = tag
+          :h3 (recur next-els (text el) table-map)
+          :h4 (recur next-els (text el) table-map)
+          :table (recur next-els last-header (assoc table-map last-header el))
+          (recur next-els last-header table-map))))))
 
 (defprocessor :pokemon-list
   :process-fn (fn [doc ctx]
@@ -15,9 +36,9 @@
                           :processor :pokemon-detail}) rows))))
 
 (defprocessor :pokemon-detail)
-; TODO: write this processor!
-;   :process-fn (fn [doc ctx]
-                ; {:name (:name ctx)}))
+  :process-fn (fn [doc ctx]
+                {:name (:name ctx)
+                 :types (map text (select doc type-selector))})
 
 (defn run-scrape []
   (core/scrape seed :sleep 1000 :parallelism 1))
